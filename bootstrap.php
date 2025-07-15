5da9b5d6a1c6ceca7905451e94292716503400e2
nriveraaa <202310167@fit.edu.ph>
2025-07-16 06:59:24 +0800

    fvgbng

--- bootstrap.php
+++ bootstrap.php
@@ -7,7 +7,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once APP_ROOT . '/staticData/products.staticdata.php';
+require_once APP_ROOT . '/staticDatas/products.staticdata.php';
 
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
